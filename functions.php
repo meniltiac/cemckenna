aa6783b7c188d6d676ae888512a9841513f3128d
Caitlin McKenna <caitlin.elizamckenna@gmail.com>
2016-08-22 22:19:55 -0600

    Adding in bootstrap 4 to new theme

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/* LOAD STYLESHEETS
+================================================== */
+if (!function_exists('sf_enqueue_styles')) {
+    function sf_enqueue_styles() {
+
+        wp_enqueue_style('bootstrap', '/wp-content/themes/cemckenna/bootstrap-4.0.0-alpha.3/dist/css/bootstrap.css', array(), NULL, 'all');
+
+    }
+    add_action('wp_enqueue_scripts', 'sf_enqueue_styles', 99);
+}
